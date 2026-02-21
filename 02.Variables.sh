3753f0c4918966f69a4d1005d29d113fd08823b1
VinayKumar <vinaydarla832@gmail.com>
2026-02-21 21:14:36 +0530

    Adding hellow world script

---
+++ 02.Variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Variables
+Name1="Raja"
+Name2="Vinay"
+echo "$Name2 ::haow are you doing $Name1"
+echo "$Name1 :: Iam doing good how are you $Name2"
+echo "$Name2 :: Iam doing good too $Name1"
